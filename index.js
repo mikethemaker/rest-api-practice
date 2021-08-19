7b6eda182673c0e8fa2e9e18cd9fb1166189a049
Michael Wilson <keyfingrs@gmail.com>
2021-08-18 17:40:58 -0700

    test
    added some test to the test

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+const express = require('express')
+
+const app = express()
+
+app.get("/", (req, res) => {
+    res.json({ hello: "world!" })
+  })
+
 // test file is a test file
+console.log("poop complete!");
